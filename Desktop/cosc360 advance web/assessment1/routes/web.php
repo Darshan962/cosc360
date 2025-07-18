e5c8ebd49405642a0e6a2bfe6ae8a625c72ef12a
Darshan Baniya <dbaniya@myune.edu.au>
2025-07-19 00:37:01 +1000

    Defining blog post CRUD route in web.php

--- Desktop/cosc360 advance web/assessment1/routes/web.php
+++ Desktop/cosc360 advance web/assessment1/routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 Route::get('/', function () {
     return view('home');
 });
+
+// Blog CRUD Routes
+Route::get('/all', [PostController::class, 'index']);
+Route::get('/create', [PostController::class, 'create']);
+Route::post('/save', [PostController::class, 'store']);
+Route::get('/edit/{id}', [PostController::class, 'edit']);
+Route::post('/update/{id}', [PostController::class, 'update']);
+Route::get('/delete/{id}', [PostController::class, 'destroy']);
