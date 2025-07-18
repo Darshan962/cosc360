ffb29e4812e005b9902db3bccb554e9b31865322
Darshan Baniya <dbaniya@myune.edu.au>
2025-07-19 01:05:49 +1000

    Use named routes for blog CRUD

--- Desktop/cosc360 advance web/assessment1/routes/web.php
+++ Desktop/cosc360 advance web/assessment1/routes/web.php
@@ -8,9 +8,20 @@ Route::get('/', function () {
 });
 
 // Blog CRUD Routes
-Route::get('/all', [PostController::class, 'index']);
-Route::get('/create', [PostController::class, 'create']);
-Route::post('/save', [PostController::class, 'store']);
-Route::get('/edit/{id}', [PostController::class, 'edit']);
-Route::post('/update/{id}', [PostController::class, 'update']);
-Route::get('/delete/{id}', [PostController::class, 'destroy']);
+// List all posts
+Route::get('/all', [PostController::class, 'index'])->name('posts.index');
+
+// Create post form
+Route::get('/create', [PostController::class, 'create'])->name('posts.create');
+
+// Store new post
+Route::post('/store', [PostController::class, 'store'])->name('posts.store');
+
+// Edit post form
+Route::get('/edit/{id}', [PostController::class, 'edit'])->name('posts.edit');
+
+// Update post
+Route::post('/update/{id}', [PostController::class, 'update'])->name('posts.update');
+
+// Delete post
+Route::get('/delete/{id}', [PostController::class, 'destroy'])->name('posts.destroy');
